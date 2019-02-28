7bf6528086f7a0e9ebd2c1c6f8a455fed769e9cb
jerry <code.ci@qq.com>
2019-02-28 17:12:48 +0800

    init

---
+++ src/main/java/com/rejerry/javagist/JavagistApplication.java
@@ -0,0 +1,13 @@
+package com.rejerry.javagist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavagistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavagistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rejerry/javagist/JavagistApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rejerry.javagist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavagistApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
