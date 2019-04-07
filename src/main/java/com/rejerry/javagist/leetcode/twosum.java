b400a5d186aea9ac2a89e348f2443d0c3bbd4cd7
jerry <code.ci@qq.com>
2019-04-08 00:59:34 +0800

    leet code q1: two sum update

--- src/main/java/com/rejerry/javagist/leetcode/twosum.java
+++ src/main/java/com/rejerry/javagist/leetcode/twosum.java
@@ -1,39 +1,38 @@
 package com.rejerry.javagist.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class twosum {
     public static void main(String[] args) {
-        int[] nums = new int[]{2, 7, 11, 15};
-        int target = 9;
+//        int[] nums = new int[]{2, 7, 11, 15};
+//        int target = 9;
+//        int[] nums = new int[]{0, 4, 3, 0};
+//        int target = 0;
+        int[] nums = new int[]{-3, 4, 3, 90};
+        int target = 0;
         int[] res = twoSum(nums, target);
         System.out.println("res[0] = " + res[0]);
         System.out.println("res[1] = " + res[1]);
     }
 
     private static int[] twoSum(int[] nums, int target) {
-        List<Integer> include2 = new ArrayList<>(nums.length);
-
-        for (int num : nums) {
-            if (num < target) {
-                include2.add(num);
-            }
-        }
-
-        int[] include = new int[include2.size()];
-        for (int i = 0; i < include2.size(); i++) {
-            include[i] = include2.get(i);
-        }
-
         boolean find = false;
         int[] res = new int[2];
-        for (int i = 0; i < include.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (find) break;
-            for (int j = 0; j < include.length && i!=j; j++) {
-                if(include[i] + include[j] == target){
-                    res[0] = i;
-                    res[1] = j;
+
+//            if (nums[i] > target) continue;
+
+            for (int j = 1; j < nums.length && i != j; j++) {
+//                if (nums[j] > target) continue;
+
+                if (nums[i] + nums[j] == target) {
+                    if (i < j) {
+                        res[0] = i;
+                        res[1] = j;
+                    } else {
+                        res[1] = i;
+                        res[0] = j;
+                    }
+
                     find = true;
                     break;
                 }
